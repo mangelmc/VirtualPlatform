be8030b68aa5fcbc5b727ca9aac474542e083d88
“leonardo6589157” <leo273000@gmail.com>
2017-06-16 15:26:42 -0400

    Creacion de cursos

--- server/main.js
+++ server/main.js
@@ -2,4 +2,16 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  Meteor.methods({
+  	"insertCurso": function(title,desc){
+            if(Meteor.userId()){ 
+                CURSOS.insert({titulo: title,descripcion:desc,owner:this.userId});
+                return true;
+            }
+        }
+  });
+
+  Meteor.publish("getCursos",function(){
+		return CURSOS.find({owner:this.userId});
+	  });
 });
