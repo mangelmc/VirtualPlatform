66732e20a233feaa46e97c5e031434b7654c3590
Leonardo <leo273000@gmail.com>
2017-06-30 01:31:11 -0400

    Chat x material funcionando y control roles registro arreglado

--- client/cursos/chat.js
+++ client/cursos/chat.js
@@ -3,5 +3,43 @@ Template.chat.events({
 		$('#chatlayout').slideToggle('slow');
 		$('#asklayout').slideToggle('slow');
 	},
-	
+	'submit #chatear': function (e) {
+		e.preventDefault();
+		obj = {
+			idUs : Meteor.userId(),
+			texto : e.target.texto.value,
+			idMat : FlowRouter.getQueryParam('idMat'),
+		}
+		//console.log(obj);
+		Meteor.call('insertMensaje', obj);
+		e.target.texto.value='';
+	}
+});
+Template.chat.helpers({
+	listMensajes: function () {
+		return MENSAJES.find({}).fetch().reverse();
+	},
+	namChat : function () {
+		//console.log(nameChat);
+		return nameChat.get();
+
+	},
+
+});
+Template.mensaje.helpers({
+	idMi: function () {
+		if (Meteor.userId()==this.idUs) {
+			return true;
+		}
+		return false;
+
+	}
+});
+Template.mensaje.events({
+	'click .elimensaje': function () {
+		var conf = confirm('Seguro que quiere eliminar este mensaje');
+		if (conf==true) {
+			Meteor.call('eliMensaje', this._id);
+		}
+	}
 });
